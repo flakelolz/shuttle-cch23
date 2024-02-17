1f105ac229cc67caa02442087c30e7618bae31c0
Michael Gonzalez <michael_gonzalez03@hotmail.com>
2024-02-17 03:44:58 -0400

    Day 05 that came in late

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,2 +1,55 @@
-// NOTE: The Grinch stole the contents of this challenge while the Shuttle team wasn't looking!
-// Check back soon in case we find the challenge contents!
+use axum::{extract::Query, Json};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+pub struct Pagination {
+    offset: Option<usize>,
+    limit: Option<usize>,
+    split: Option<usize>,
+}
+
+#[derive(Serialize)]
+#[serde(untagged)]
+pub enum Value {
+    Default(Vec<String>),
+    Splitted(Vec<Vec<String>>),
+}
+
+pub async fn pagination(
+    Query(pagination): Query<Pagination>,
+    Json(payload): Json<Vec<String>>,
+) -> Json<Value> {
+    let start = if let Some(offset) = pagination.offset {
+        if offset > payload.len() {
+            payload.len()
+        } else {
+            offset
+        }
+    } else {
+        0
+    };
+
+    let end = if let Some(limit) = pagination.limit {
+        if start + limit > payload.len() {
+            payload.len()
+        } else {
+            start + limit
+        }
+    } else {
+        payload.len()
+    };
+
+    let names = payload[start..end].to_vec();
+
+    if let Some(split) = pagination.split {
+        let response = names
+            .chunks(split)
+            .map(|chunk| chunk.to_vec())
+            .collect::<Vec<Vec<String>>>();
+        return Json(Value::Splitted(response));
+    }
+
+    let response = names.iter().map(|s| s.to_string()).collect::<Vec<String>>();
+
+    Json(Value::Default(response))
+}
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,8 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .route("/1/*nums", get(day01::power_nums))
         .route("/4/strength", post(day04::calc_strength))
         .route("/4/contest", post(day04::contest))
-        .route("/6", post(day06::elfs_on_shelf));
+        .route("/5", post(day05::pagination))
+        .route("/6", post(day06::elfs_on_shelf))
 
     Ok(router.into())
 }
